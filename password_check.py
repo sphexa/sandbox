51557174379d397ec28dd4fc7f96bcbab37af0ee
sphex <yujian.ao@my.jcu.edu.au>
2022-04-05 11:09:25 +0800

    Add password_check program

---
+++ password_check.py
@@ -0,0 +1,45 @@
+minimum_length = 4
+password = input("Enter password at least {} characters: ".format(minimum_length))
+while len(password) < minimum_length:
+    print("Passord too short")
+    password = input("Enter password at least {} characters: ".format(minimum_length))
+
+print('*' * len(password))
+
+
+# MINIMUM_LENGTH = 4
+#
+#
+# def version_1():
+#     """Get a password of valid size and print asterisks."""
+#     password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+#     while len(password) < MINIMUM_LENGTH:
+#         password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+#
+#     print('*' * len(password))
+#
+#
+# # version_1()
+#
+#
+# def main():
+#     """Get and print password using functions."""
+#     password = get_password(MINIMUM_LENGTH)
+#     print_asterisks(password)
+#
+#
+# def get_password(minimum_length):
+#     """Get password, ensuring it meets the minimum_length requirement."""
+#     password = input("Enter password of at least {} characters: ".format(minimum_length))
+#     while len(password) < minimum_length:
+#         print("Password too short")
+#         password = input("Enter password of at least {} characters: ".format(minimum_length))
+#     return password
+#
+#
+# def print_asterisks(sequence):
+#     """Print as many asterisks as there are characters in the passed-in sequence."""
+#     print('*' * len(sequence))
+#
+#
+# main()
